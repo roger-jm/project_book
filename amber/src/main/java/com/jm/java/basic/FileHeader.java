b2908c627881b7b1f8fe07aafe5a017e8eebc2da
wangmingjie <jmwmj208@163.com>
2015-07-19 12:17:19 +0800

    File header

---
+++ amber/src/main/java/com/jm/java/basic/FileHeader.java
@@ -0,0 +1,12 @@
+package com.jm.java.basic;
+
+/**
+ * This is Porject project_book
+ * Created by jmwmj
+ * On 2015/7/19
+ */
+public class FileHeader {
+    public static void main(String[] args) {
+
+    }
+}
