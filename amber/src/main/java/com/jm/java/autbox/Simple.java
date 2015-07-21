9ed6ab73d987994b5e1f65852474798e7005a268
Roger Wang <jmwmj208@live.com>
2015-07-21 16:19:34 +0800

    box and gc 1

---
+++ amber/src/main/java/com/jm/java/autbox/Simple.java
@@ -0,0 +1,15 @@
+package com.jm.java.autbox;
+
+/**
+ * This is Project project_book
+ * Created by jmwmj
+ * On 2015/7/20
+ * At 9:22
+ */
+public class Simple {
+
+    public void freshmen(){
+        int a = 0;
+
+    }
+}
